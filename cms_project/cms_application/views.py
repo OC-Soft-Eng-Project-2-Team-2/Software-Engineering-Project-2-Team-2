69ca74e76e519173a809ca82c688fbf3f6d74183
pcaheza <54516709+pcaheza@users.noreply.github.com>
2020-07-16 22:38:10 -0500

    login and authentication

--- cms_project/cms_application/views.py
+++ cms_project/cms_application/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User,auth
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
-
-def home(response):
-    return HttpResponse("<h1>Class Management System</h1>")
+@login_required
+def home(request):
+    return render(request,"home.html")
     
-def login(response):
-    return HttpResponse("<h1>Login</h1>")
+
+
+# def login(request):
+#     if request.method=='POST':
+#         username=request.POST['username']
+#         password=request.POST['password']
+        
+#         user=auth.authenticate(username=username,password=password)
+
+
+#         return render(request,"login.html")
+        
--- cms_project/cms_project/urls.py
+++ cms_project/cms_project/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("cms_application.urls")),
-    path('home/', include("cms_application.urls")),
+   # path('home/', include("cms_application.urls")),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
