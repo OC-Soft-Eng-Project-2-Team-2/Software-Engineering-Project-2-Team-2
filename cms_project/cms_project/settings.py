6f45e4b41a1e1e6602b6f98eace511a8ed7a43ca
pcaheza <54516709+pcaheza@users.noreply.github.com>
2020-07-20 19:17:46 -0500

    loginPage

--- cms_project/cms_project/settings.py
+++ cms_project/cms_project/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cms_application'
+    'cms_application',
+    'crispy_forms'
 ]
+CRSISPY_TEMPLATE_PACK='bootstrap4.5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -123,3 +125,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
 #LOGOUT_REDIRECT_URL = '/accounts/logout' 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
