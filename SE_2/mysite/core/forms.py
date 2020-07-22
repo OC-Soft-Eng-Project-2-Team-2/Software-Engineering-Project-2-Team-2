37b45bff1f71bd61ff35a5bb853f3765da866a59
Vraj Patel <51841184+vrajpatell@users.noreply.github.com>
2020-07-22 17:49:26 -0500

    Delete forms.py

--- SE_2/mysite/core/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from .models import Assignment
-
-
-class BookForm(forms.ModelForm):
-    class Meta:
-        model = Assignment
-        fields = ('title', 'author', 'pdf')
