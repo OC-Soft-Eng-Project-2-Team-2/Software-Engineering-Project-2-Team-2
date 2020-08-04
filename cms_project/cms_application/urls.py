80f39851a549974b26099bcc707d8f787f2d1d32
tylerdavidjoy <tyler.david.joy@eagles.oc.edu>
2020-08-04 17:27:02 -0500

    Modified urls link

--- cms_project/cms_application/urls.py
+++ cms_project/cms_application/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("", views.login, name="login"),
     path("home/", views.home, name="home"),
     path("class/", views.aClass, name="class"),
     path("grades/", views.grades, name="grades"),
