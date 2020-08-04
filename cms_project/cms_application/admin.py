3cc1951568484ea078fde98e72841226ea890d1a
pcaheza <54516709+pcaheza@users.noreply.github.com>
2020-08-03 19:11:33 -0500

    creating REST API

--- cms_project/cms_application/admin.py
+++ cms_project/cms_application/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Student,Professor,Admin,Course
+
 # Register your models here.
+admin.site.register(Student)
+admin.site.register(Professor)
+admin.site.register(Admin)
+admin.site.register(Course)
