861d39836f5b902b06d4ffb9bcfcd85a5b1ad086
tylerdavidjoy <tyler.david.joy@eagles.oc.edu>
2020-08-05 18:26:56 -0500

    Fixed authentication issue that was caused by a redirect.

--- cms_project/cms_application/urls.py
+++ cms_project/cms_application/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.login, name="login"),
+    path("", views.login, name="logn"), //Do not correct the spelling, login is reserved so I used logn
     path("home/", views.home, name="home"),
     path("class/", views.aClass, name="class"),
     path("grades/", views.grades, name="grades"),
