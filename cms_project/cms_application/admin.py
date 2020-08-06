064f00ec2a6af87a3232737eb15fe059b5d3617d
jme98 <jennifer.easter@eagles.oc.edu>
2020-08-06 12:54:02 -0500

    merge cleanup

--- cms_project/cms_application/admin.py
+++ cms_project/cms_application/admin.py
@@ -1,15 +1,6 @@
 from django.contrib import admin
 from .models import Student, Professor, Course, Section, Assignment, Announcement, Enrollment, StudentAssignment, CalendarEvent
 
-<<<<<<< Updated upstream
-from .models import Student,Professor,Admin,Course
-
-# Register your models here.
-admin.site.register(Student)
-admin.site.register(Professor)
-admin.site.register(Admin)
-admin.site.register(Course)
-=======
 admin.site.register(Student)
 
 admin.site.register(Professor)
@@ -27,4 +18,3 @@ admin.site.register(Enrollment)
 admin.site.register(StudentAssignment)
 
 admin.site.register(CalendarEvent)
->>>>>>> Stashed changes
