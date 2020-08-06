9210edb283bd041eda34c51cbaabf34037f4d63a
tylerdavidjoy <tyler.david.joy@eagles.oc.edu>
2020-08-05 19:38:57 -0500

    Modified urls and views so that you can go directly to any page from login instead of being limited to the home page.

--- cms_project/cms_application/views.py
+++ cms_project/cms_application/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
-#@login_required
+@login_required
 def test(request):
     return render(request, "cms_application/home.html")
 
@@ -81,7 +81,7 @@ def home(request):
     return render(request, "cms_application/home.html", context)
     
 def login(response):
-    return redirect('/login/')
+    return redirect('/login/?next=/home/')
 
 @login_required   
 def aClass(response):
