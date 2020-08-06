95f4ddffd8b29a34e910dc963629d1fa4b5f9113
jme98 <jennifer.easter@eagles.oc.edu>
2020-08-06 12:53:50 -0500

    merged master into branch 'jenn'

--- cms_project/cms_application/admin.py
+++ cms_project/cms_application/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from .models import Student, Professor, Course, Section, Assignment, Announcement, Enrollment, StudentAssignment, CalendarEvent
 
+<<<<<<< Updated upstream
 from .models import Student,Professor,Admin,Course
 
 # Register your models here.
@@ -7,3 +9,22 @@ admin.site.register(Student)
 admin.site.register(Professor)
 admin.site.register(Admin)
 admin.site.register(Course)
+=======
+admin.site.register(Student)
+
+admin.site.register(Professor)
+
+admin.site.register(Course)
+
+admin.site.register(Section)
+
+admin.site.register(Assignment)
+
+admin.site.register(Announcement)
+
+admin.site.register(Enrollment)
+
+admin.site.register(StudentAssignment)
+
+admin.site.register(CalendarEvent)
+>>>>>>> Stashed changes
--- cms_project/cms_application/models.py
+++ cms_project/cms_application/models.py
@@ -131,18 +131,6 @@ class Professor(models.Model):
         return self.last_name + ', ' + self.first_name + ' (' + self.faculty_id + '); ' + self.department
 
 
-class Admin(models.Model):
-    user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True)
-
-    staff_id = models.CharField(max_length=7, primary_key=True)
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
-    email = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.last_name + ', ' + self.first_name + ' (' + self.staff_id + '); Administrator'
-
-
 class Course(models.Model):
     course_name = models.CharField(max_length=100)
     department_code = models.CharField(max_length=4)
